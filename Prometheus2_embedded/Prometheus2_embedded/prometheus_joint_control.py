667df2c3f47dbec1e3de3340cbb671bfd82f3d6b
Yannis Vlachodimos <144154225+Gl4ukos@users.noreply.github.com>
2026-03-02 19:11:57 +0200

    Added Led interface for state display

---
+++ Prometheus2_embedded/Prometheus2_embedded/prometheus_joint_control.py
@@ -0,0 +1,37 @@
+from machine import Pin, ADC, PWM
+import time
+from servo_config import *
+
+        
+# === ADC INPUT FOR SERVO CONTROl
+adc_pins = [ADC(26), ADC(27), ADC(28)]
+pots = [0,0,0]
+
+def read_voltage():
+    global adc_pins
+    global pots
+    
+    DEADBAND = 0.01
+    for i in range(len(adc_pins)):
+        raw = adc_pins[i].read_u16()
+        voltage_normalized = raw / 65535
+        if abs(voltage_normalized - pots[i]) > DEADBAND:
+            pots[i] = 0.3*voltage_normalized + 0.7*pots[i]
+            
+read_voltage()
+time.sleep(1)
+read_voltage()
+# === SERVO SETUP
+servos = [servo(servo_arm_config), servo(servo_forearm_config), servo(servo_wrist_config)]
+
+# === MAIN LOOP
+print_index = 0
+    
+while True:
+    read_voltage()
+    for i in range(len(pots)):
+        servos[i].update_servo_from_pot(pots[i])
+    if(print_index%10==0):
+        print(pots)
+    print_index += 1
+    time.sleep(0.01)
